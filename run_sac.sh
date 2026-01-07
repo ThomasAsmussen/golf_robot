9c198d9eabb3fd62a492734bbad5007ca74b2ea8
fast2day <martinschaarup@live.dk>
2026-01-07 12:54:14 +0100

    back to venv

--- run_sac.sh
+++ run_sac.sh
@@ -16,5 +16,5 @@ echo "Running script..."
 
 module load cuda/11.8
 module load python3/3.10.13
-source golf_env/bin/activate
+source golf_venv/bin/activate
 python3 /zhome/85/0/156431/golf_robot/src/golf_robot/SAC_bandit.py > log/run_sac$(date +"%d-%m-%y")_$(date +'%H:%M:%S').log
