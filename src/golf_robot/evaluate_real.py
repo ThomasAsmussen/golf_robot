9daeca3b11a4dcaee32c4db93f9a55047c3c1b0b
Thomas Asmussen <tjasmussen@gmail.com>
2026-01-12 15:42:54 +0100

    evaluate real bug fix

--- src/golf_robot/evaluate_real.py
+++ src/golf_robot/evaluate_real.py
@@ -31,11 +31,12 @@ from hand_designed_agent import hand_tuned_policy
 OPERATING_SYSTEM = "linux"  # "windows" or "linux"
 CAMERA_INDEX_START = 4  # starting camera index for real robot
 CAMERA_INDEX_END   = 2  # ending camera index for real robot
+actor_name = "hand_tuned_policy"
 # CAMERA_END = r'@device_pnp_\\?\usb#vid_046d&pid_08e5&mi_00#7&23aa88cc&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global'
 #CAMERA_END = r'@device_pnp_\\?\usb#vid_046d&pid_08e5&mi_00#8&2e31d80&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global'
 #CAMERA_END = r'@device_pnp_\\?\usb#vid_046d&pid_08e5&mi_00#8&2e31d80&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global'
 END_POS = [-2.47, -2.38, -1.55, 1.66, 0.49, -0.26]
-
+LOG_SHOTS = False
 
 def real_init_parameters(camera_index, chosen_hole=None):
     # Ball
@@ -49,7 +50,7 @@ def real_init_parameters(camera_index, chosen_hole=None):
     # Holes
     if chosen_hole is None:
         # chosen_hole = random.choice([1,2,3])
-        chosen_hole = 3
+        chosen_hole = 2
     # chosen_hole = 1  # for testing purposes
     here = Path(__file__).resolve().parent
     config_dir = here.parents[1] / "configs"
@@ -140,159 +141,173 @@ def run_real(impact_velocity, swing_angle, ball_start_position, planner = "quint
             text=True,
         )
 
-    
     result = prompter.run_with_spinner("Shooting", run_traj)
 
     print("Trajectory streamer output:")
     print(result.stdout)
-    
+    if LOG_SHOTS:
 
-    time.sleep(2.0)
-    # Stop vision recording
-    print("Saving recording from replay buffer")
-    obs_replay_save()
-    # stop_event.set()
-    # recording_thread.join()
-    #stop_ffmpeg_record(rec)
-    
-    
-    print("Recording thread joined. Done.")
+        time.sleep(2.0)
+        # Stop vision recording
+        print("Saving recording from replay buffer")
+        obs_replay_save()
 
-    data_dir = "data/OBS_saved_replay_buffer"
-    prefix ="Replay"
-    pattern = os.path.join(data_dir, f"{prefix}*")
-    # print(pattern)
-    files = glob.glob(pattern)
-    # print(files)
-    video_path = max(files, key=os.path.getmtime)
-    
-    print("Recorded video path:", video_path)
-    # frames_captured = get_video_frame_count(video_path)
-    # expected_frames = int(15 * 30)
-    # print(frames_captured)
+        print("Recording thread joined. Done.")
 
-    
-    # print(f"Captured {frames_captured}/{expected_frames} frames")
-    # print(f"Drop ratio: {(1 - frames_captured/expected_frames)*100:.1f}%")
-    
-    # Start thread for return to home position
+        data_dir = "data/OBS_saved_replay_buffer"
+        prefix ="Replay"
+        pattern = os.path.join(data_dir, f"{prefix}*")
+        # print(pattern)
+        files = glob.glob(pattern)
+        # print(files)
+        video_path = max(files, key=os.path.getmtime)
+        
+        print("Recorded video path:", video_path)
+
     ur_movej(
         robot_ip="192.38.66.227",
         q=END_POS,
     )
     print("Return to home position command sent.")
 
-    # Measure 
-    # Deafaults
-    dist_at_hole = None
-    speed_at_hole = None
-    ball_final_position = np.array([0.0, 0.0])  # optional: safe default
-    continue_evaluation = False
-    on_green = False
-    
-    state = prompter.ask_hole_oob(chosen_hole)
-
-    wrong_hole = False
-
-    if state == "in_hole":
-        print("Ball in hole confirmed")
-        in_hole = True
-        out_of_bounds = False
-        dist_at_hole, speed_at_hole = None, None
-        here = Path(__file__).resolve().parent
-        config_dir = here.parents[1] / "configs"
-        with open(config_dir / "hole_config.yaml", "r") as f:
-            hole_positions = yaml.safe_load(f)
-        # hole_number = 1  # choose hole number
-        hx = hole_positions[chosen_hole]["x"]
-        hy = hole_positions[chosen_hole]["y"]
-        ball_final_position = np.array([hx, hy]) # choose first hole for now
-
-    elif state == "oob":
-        print("Ball out of bounds confirmed")
-        in_hole = False
-        out_of_bounds = True
-
-    elif state == "wrong_hole":
-        chosen_hole = prompter.ask_which_hole()
-        print(f"User indicated ball went to wrong hole, new hole is {chosen_hole}")
-        in_hole = True
-        out_of_bounds = False
-        wrong_hole = chosen_hole
-
-    else:
-        in_hole = False
-        out_of_bounds = False
-        
-    if not out_of_bounds and not in_hole:
-        on_green = prompter.ask_yes_no("Is ball on green?")
-        if on_green:
-            obs_screenshot()
-        #   ball_final_position = get_ball_final_position(camera_index=CAMERA_INDEX_END, chosen_hole=chosen_hole, use_cam=True, debug=True, operating_system=OPERATING_SYSTEM)
-            ball_final_position = get_ball_final_position(camera_index=CAMERA_INDEX_END, chosen_hole=chosen_hole, use_cam=False, debug=True, operating_system=OPERATING_SYSTEM)
-        else: 
-            # data_dir = "data"
-            # prefix ="trajectory_recording"
-            # pattern = os.path.join(data_dir, f"{prefix}_*_last15s.avi")
-            # files = glob.glob(pattern)
-            # video_path = max(files, key=os.path.getmtime)
-            compute_all_holes = False
-            traj_3_worked = False
-            if compute_all_holes:
-                dist_at_hole = []
-                speed_at_hole = []
-                
-                for i in range(1,4):
-                    dist_at_hole_tmp, speed_at_hole_tmp, xs, ys, hole_xo, hole_yo, bx, by = process_video(
-                        video_path, chosen_hole=i, real_time_show=False, compute_all_holes=False
-                    )
-                    dist_at_hole.append(dist_at_hole_tmp)
-                    speed_at_hole.append(speed_at_hole_tmp)
-                print("Distance at hole list:", dist_at_hole)
-                print("Speed at hole list:", speed_at_hole)
-                if dist_at_hole[2] is not None:
-                    traj_3_worked = True
-
-            else:
-                dist_at_hole, speed_at_hole, xs, ys, hole_xo, hole_yo, bx, by = process_video(
-                    video_path, chosen_hole=chosen_hole, real_time_show=False, compute_all_holes=False
-                )
-            if dist_at_hole is not None and traj_3_worked:
-                prompter.show_trajectory_plot(xs, ys, hole_xo, hole_yo, bx, by)
-            #dist_at_hole, speed_at_hole = process_video(
-            #    video_path, chosen_hole=chosen_hole,
-            #    real_time_show=False,   # turn off video if running batch
-            #)
-            ball_final_position = np.array([0, 0]) # dummy value
-
-    
-    if out_of_bounds:
-        ball_final_position = np.array([0, 0]) # dummy value
+    if LOG_SHOTS:
+        # Measure 
+        # Deafaults
         dist_at_hole = None
         speed_at_hole = None
+        ball_final_position = np.array([0.0, 0.0])  # optional: safe default
+        continue_evaluation = False
+        on_green = False
+        
+        state = prompter.ask_hole_oob(chosen_hole)
 
+        wrong_hole = False
+
+        if state == "in_hole":
+            print("Ball in hole confirmed")
+            in_hole = True
+            out_of_bounds = False
+            dist_at_hole, speed_at_hole = None, None
+            here = Path(__file__).resolve().parent
+            config_dir = here.parents[1] / "configs"
+            with open(config_dir / "hole_config.yaml", "r") as f:
+                hole_positions = yaml.safe_load(f)
+            # hole_number = 1  # choose hole number
+            hx = hole_positions[chosen_hole]["x"]
+            hy = hole_positions[chosen_hole]["y"]
+            ball_final_position = np.array([hx, hy]) # choose first hole for now
+
+        elif state == "oob":
+            print("Ball out of bounds confirmed")
+            in_hole = False
+            out_of_bounds = True
+
+        elif state == "wrong_hole":
+            chosen_hole = prompter.ask_which_hole()
+            print(f"User indicated ball went to wrong hole, new hole is {chosen_hole}")
+            in_hole = True
+            out_of_bounds = False
+            wrong_hole = chosen_hole
+
+        else:
+            in_hole = False
+            out_of_bounds = False
+            
+        if not out_of_bounds and not in_hole:
+            on_green = prompter.ask_yes_no("Is ball on green?")
+            if on_green:
+                obs_screenshot()
+                ball_final_position = get_ball_final_position(camera_index=CAMERA_INDEX_END, chosen_hole=chosen_hole, use_cam=False, debug=True, operating_system=OPERATING_SYSTEM)
+            
+            else: 
+                compute_all_holes = False
+                traj_3_worked = False
+                if compute_all_holes:
+                    dist_at_hole = []
+                    speed_at_hole = []
+                    
+                    for i in range(1,4):
+                        dist_at_hole_tmp, speed_at_hole_tmp, xs, ys, hole_xo, hole_yo, bx, by = process_video(
+                            video_path, chosen_hole=i, real_time_show=False, compute_all_holes=False
+                        )
+                        dist_at_hole.append(dist_at_hole_tmp)
+                        speed_at_hole.append(speed_at_hole_tmp)
+                    print("Distance at hole list:", dist_at_hole)
+                    print("Speed at hole list:", speed_at_hole)
+                    if dist_at_hole[2] is not None:
+                        traj_3_worked = True
+
+                else:
+                    dist_at_hole, speed_at_hole, xs, ys, hole_xo, hole_yo, bx, by = process_video(
+                        video_path, chosen_hole=chosen_hole, real_time_show=False, compute_all_holes=False
+                    )
+                if dist_at_hole is not None and traj_3_worked:
+                    prompter.show_trajectory_plot(xs, ys, hole_xo, hole_yo, bx, by)
+
+                ball_final_position = np.array([0, 0]) # dummy value
+
+        if out_of_bounds:
+            ball_final_position = np.array([0, 0]) # dummy value
+            dist_at_hole = None
+            speed_at_hole = None
+   
+        used_for_training = prompter.ask_yes_no("Log shot for evaluation?")
+        if not used_for_training:
+            print("Shot discarded by user")
+
+
+        continue_evaluation = prompter.ask_yes_no("Continue evaluation?")
+        if not continue_evaluation:
+            print("Training aborted by user")
+        
+        meta = {
+            "dist_at_hole": dist_at_hole,
+            "speed_at_hole": speed_at_hole,
+            "used_for_training": used_for_training,
+            "continue_evaluation": continue_evaluation,
+            "out_of_bounds": out_of_bounds,
+            "wrong_hole": wrong_hole,
+            "on_green": on_green,
+        }
     
-    used_for_training = prompter.ask_yes_no("Use shot for training?")
-    if not used_for_training:
-        print("Shot discarded by user")
+    else:
+        continue_evaluation = prompter.ask_yes_no("Continue evaluation?")
+        if not continue_evaluation:
+            print("Evaluation aborted by user")
 
+        meta = {
+            "dist_at_hole": None,
+            "speed_at_hole": None,
+            "used_for_training": False,
+            "continue_evaluation": continue_evaluation,
+            "out_of_bounds": False,
+            "wrong_hole": False,
+            "on_green": False,
+        }
 
-    continue_evaluation = prompter.ask_yes_no("Continue training?")
-    if not continue_evaluation:
-        print("Training aborted by user")
-    
-    meta = {
-        "dist_at_hole": dist_at_hole,
-        "speed_at_hole": speed_at_hole,
-        "used_for_training": used_for_training,
-        "continue_evaluation": continue_evaluation,
-        "out_of_bounds": out_of_bounds,
-        "wrong_hole": wrong_hole,
-        "on_green": on_green,
-    }
     print("meta:", meta)
     return ball_final_position[0], ball_final_position[1], in_hole, meta
 
+
+def load_correct_actor(actor_name, rl_cfg):
+    # actor, device = load_actor(rl_cfg["training"]["model_name"], rl_cfg)
+    if actor_name == "hand_tuned_policy":
+        print("Using hand-tuned policy")
+        return hand_tuned_policy, None
+
+    elif actor_name == "SAC_bandit":
+        from SAC_bandit import load_actor
+        print("Using SAC bandit policy")
+        
+    elif actor_name == "thompson_bandit":
+        from thompson_bandit import load_actor
+        print("Using Thompson bandit policy")
+    
+    elif actor_name == "contextual_bandit2":
+        from contextual_bandit2 import load_actor
+        print("Using Contextual bandit v2 policy")
+
+    return load_actor(actor_name, rl_cfg)
 
 def main():
     global prompter
@@ -301,7 +316,8 @@ def main():
 
     here = Path(__file__).resolve().parent
 
-    obs_replay_start()
+    if LOG_SHOTS:
+        obs_replay_start()
 
     project_root       = here.parents[1]
     mujoco_config_path = project_root / "configs" / "mujoco_config.yaml"
@@ -313,9 +329,9 @@ def main():
     with open(rl_config_path, "r") as f:
         rl_cfg = yaml.safe_load(f)
 
-    # actor, device = load_actor(rl_cfg["training"]["model_name"], rl_cfg)
-
-    actor = hand_tuned_policy
+    actor, device = load_correct_actor(actor_name, rl_cfg)
+    
+    # actor = hand_tuned_policy
     try:
         evaluation_policy_hand_tuned(actor, mujoco_cfg, rl_cfg, num_episodes=10, max_num_discs=0, env_step=run_real, env_type="real", input_func=real_init_parameters)
     finally:
