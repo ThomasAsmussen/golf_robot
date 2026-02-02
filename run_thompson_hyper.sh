b34c9f2a46af8c5d7df673f56cfdf31432121950
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-02 23:23:54 +0100

    Fixed .sh error

--- run_thompson_hyper.sh
+++ run_thompson_hyper.sh
@@ -19,4 +19,4 @@ source golf_venv/bin/activate
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 10 rl_golf/golf_robot-src_golf_robot_reinforcement_learning/bpoi2v57
+wandb agent --count 10 rl_golf/golf_robot-src_golf_robot/2dkbdkh9
