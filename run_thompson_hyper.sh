2c8f8270f515f067f2f8ad6f5086892b6db2fcd2
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-04 14:10:10 +0100

    Sweep 0 discs no noise

--- run_thompson_hyper.sh
+++ run_thompson_hyper.sh
@@ -19,4 +19,4 @@ source golf_venv/bin/activate
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/n4mzk9ts
+wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/dd7jjczk
