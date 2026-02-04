d8b17aa6aadc738ff60479c1f7b32838563000aa
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-04 21:59:44 +0100

    sweep sac test

--- run_sac_hyper.sh
+++ run_sac_hyper.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 #BSUB -q hpc
 #BSUB -J sac
-### number of core
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
-### specify the memory needed
 #BSUB -R "rusage[mem=1GB]"
-### Number of hours needed
 #BSUB -W 23:59
-### added outputs and errors to files
-#BSUB -o outputs/Output_%J.out
-#BSUB -e outputs/Error_%J.err
+#BSUB -o outputs/sac_%J.out
+#BSUB -e outputs/sac_%J.err
+#BSUB -N
 
 echo "Running script..."
 
-#module load cuda/11.8
+module load cuda/11.8
 module load python3/3.10.13
 
 # Activate your env
 cd ~/golf_robot
 source golf_venv/bin/activate
 
+export WANDB_CONSOLE=off
+export WANDB_DISABLE_CODE=true
+export WANDB_START_METHOD=thread
+export WANDB_DISABLE_STATS=true
+
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 1 rl_golf/golf_robot-src_golf_robot/vvmr4b2g
+wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/9tlfjb3a
