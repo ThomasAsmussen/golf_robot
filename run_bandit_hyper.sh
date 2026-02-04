18c2905e5e1132f9b34e78078179a0195d1f48d3
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-04 21:45:32 +0100

    3 discs

--- run_bandit_hyper.sh
+++ run_bandit_hyper.sh
@@ -25,4 +25,4 @@ export WANDB_DISABLE_STATS=true
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/i0hsl2zs
+wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/1es6xd4c
