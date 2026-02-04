a85cfb2cb12030ab1442f116c50fe020325827ea
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-04 15:27:45 +0100

    ddpg sweep 1 disc no noise

--- run_bandit_hyper.sh
+++ run_bandit_hyper.sh
@@ -18,4 +18,4 @@ source golf_venv/bin/activate
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/ospo2oxe
+wandb agent --count 5 rl_golf/golf_robot-src_golf_robot/1eq81g8u
