4d3741ac2c170b5714c419ad9db7982f800016cc
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-05 13:14:18 +0100

    3 discs

--- run_td3_hyper.sh
+++ run_td3_hyper.sh
@@ -19,4 +19,4 @@ source golf_venv/bin/activate
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 5 rl_golf/golf_robot_td3_v1/et7cgiru
+wandb agent --count 5 rl_golf/golf_robot_td3_v3/m9zmrs54
