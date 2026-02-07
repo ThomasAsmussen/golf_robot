f099ccbd2567d36cfaf4c0a10a78129c7363038c
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-07 11:10:06 +0100

    1 disc

--- run_ucb_hyper.sh
+++ run_ucb_hyper.sh
@@ -3,10 +3,11 @@
 #BSUB -J ucb_sweep
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "rusage[mem=2GB]"
 #BSUB -W 23:59
 #BSUB -o outputs/ucb_sweep_%J.out
 #BSUB -e outputs/ucb_sweep_%J.err
+#BSUB -N
 
 module load cuda/11.8
 module load python3/3.10.13
@@ -17,4 +18,4 @@ source golf_venv/bin/activate
 
 # Run the wandb sweep agent
 # --count controls how many trials this job will run
-wandb agent --count 3 rl_golf/golf_robot_ucb_v_5_final_tuning/4g7l7jry
+wandb agent --count 5 rl_golf/golf_robot_ucb_v_1_final_tuning/dni5dvst
