7f4ae458d2a8dc5c9365c35ab2d068d61d77898f
Thomas Asmussen <tjasmussen@gmail.com>
2026-02-11 17:39:36 +0100

    Run UCB training final

--- run_ucb.sh
+++ run_ucb.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 #BSUB -q hpc
-#BSUB -J ucb
-### number of core
+#BSUB -J ucb_sweep
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
-### specify the memory needed
-#BSUB -R "rusage[mem=1GB]"
-### Number of hours needed
+#BSUB -R "rusage[mem=2GB]"
 #BSUB -W 23:59
-### added outputs and errors to files
-#BSUB -o outputs/Output_%J.out
-#BSUB -e outputs/Error_%J.err
-
-echo "Running script..."
+#BSUB -o outputs/ucb_sweep_%J.out
+#BSUB -e outputs/ucb_sweep_%J.err
+#BSUB -N
 
 module load cuda/11.8
 module load python3/3.10.13
+
+# Activate your env
+cd ~/golf_robot
 source golf_venv/bin/activate
 
 python3 /zhome/85/0/156431/golf_robot/src/golf_robot/ucb_bandit.py > log/run_ucb$(date +"%d-%m-%y")_$(date +'%H:%M:%S').log
